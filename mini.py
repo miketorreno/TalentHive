68f78a68d2755e7f253ea526d21592bc7ace780a
Mike <miketorreno7@gmail.com>
2024-12-13 21:53:08 +0300

    checkpoint: company creation

--- mini.py
+++ mini.py
@@ -32,7 +32,7 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
-ROLE_DECISION, J_NAME, J_EMAIL, J_LOCATION, J_REGISTER, R_NAME, R_EMAIL, R_LOCATION, R_REGISTER, R_PROFILE, BUSINESS_TYPE_CHOICE, COMPANIES, STARTUPS = range(13)
+ROLE_DECISION, J_NAME, J_EMAIL, J_LOCATION, J_REGISTER, R_NAME, R_EMAIL, R_LOCATION, R_REGISTER, R_PROFILE, BUSINESS_TYPE_CHOICE, MY_COMPANIES, COMPANIES, ADD_COMPANY_STEP_ONE, ADD_COMPANY_STEP_TWO, ADD_COMPANY_STEP_THREE, ADD_COMPANY_STEP_FOUR, ADD_COMPANY_STEP_FIVE, STARTUPS = range(19)
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
   id = update.message.chat.id
@@ -78,6 +78,7 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
             "<b>Help</b>:  get answers to your questions about Talent Hive\n\n",
       reply_markup=InlineKeyboardMarkup(jobseekerKeyboard),
     )
+    return ConversationHandler.END
   elif user[2] == 2:
     await update.message.reply_html(
       text=f"<b>Hello {user[3]} 👋 \t welcome back!</b>\n\n"
@@ -89,7 +90,7 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
             "<b>Help</b> \t-\t get answers to your questions about\n\n",
       reply_markup=InlineKeyboardMarkup(recruiterKeyboard),
     )
-    return R_PROFILE
+    return ConversationHandler.END
   else:
     await update.message.reply_html(
       '<b>Who the fuck are ya?</b>\n\n'
@@ -143,6 +144,7 @@ async def role_decision(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return ROLE_DECISION
 
 
+
 # JOB SEEKER RELATED
 async def j_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
   context.user_data['j_id'] = update.message.chat.id
@@ -175,7 +177,6 @@ async def j_location(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def j_register(update: Update, context: ContextTypes.DEFAULT_TYPE):
   print("registering...")
-
 
 async def j_profile(update: Update, context: ContextTypes.DEFAULT_TYPE):
   # id = update.effective_chat.id
@@ -238,17 +239,18 @@ async def r_profile(update: Update, context: ContextTypes.DEFAULT_TYPE):
   )
 
 async def my_companies(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  firstKeyboard = [
+  businessTypeKeyboard = [
     [InlineKeyboardButton('Company', callback_data='company')],
     [InlineKeyboardButton('Startup', callback_data='startup')],
   ]
 
-  await update.callback_query.edit_message_text(
+  await context.bot.send_message(
+    chat_id=update.effective_chat.id,
     text=f"Which represents your business better?\n\n"
           "<b>Company</b>: a classically built business in any mature, pre-existing and strong industry\n\n"
           "<b>Startup</b>: a new comer to the industry that is agile, fast-growing and has a technologically innovative approach\n\n",
-    reply_markup=InlineKeyboardMarkup(firstKeyboard),
-    parse_mode='HTML'
+    reply_markup=InlineKeyboardMarkup(businessTypeKeyboard),
+    parse_mode="HTML",
   )
 
   return BUSINESS_TYPE_CHOICE
@@ -257,20 +259,27 @@ async def business_type_choice(update: Update, context: ContextTypes.DEFAULT_TYP
   query = update.callback_query
   await query.answer()
   choice = query.data
-  
-  print(choice)
-  
+
+  companiesKeyboard = [
+    [
+      InlineKeyboardButton('My Companies', callback_data='registered_companies'),
+      InlineKeyboardButton('Add Company', callback_data='add_company')
+    ],
+    [InlineKeyboardButton('Back', callback_data='back_to_my_companies')],
+  ]
+
   if choice == 'company':
     await query.edit_message_text(
-      text=f"<b>Job Seeker Registration</b>\n\n"
-            "<b>Please enter your name: </b>",
-      parse_mode="HTML"
+      text=f"<b>My Companies:</b> view and manage companies you have already registered\n\n"
+            "<b>Add Company:</b> register a new company\n\n",
+      reply_markup=InlineKeyboardMarkup(companiesKeyboard),
+      parse_mode="HTML",
     )
     return COMPANIES
   elif choice == 'startup':
     await query.edit_message_text(
-      text=f"<b>Recruiter Registration</b>\n\n"
-            "<b>Please enter your name: </b>",
+      text=f"<b>My Startups:</b> view and manage startups you have already registered\n\n"
+            "<b>Add Startup:</b> register a new startup\n\n",
       parse_mode="HTML"
     )
     return STARTUPS
@@ -280,18 +289,156 @@ async def business_type_choice(update: Update, context: ContextTypes.DEFAULT_TYP
     )
     return BUSINESS_TYPE_CHOICE
 
-async def startups(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def companies(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  query = update.callback_query
+  await query.answer()
+  
+  if query.data == 'back_to_my_companies':
+    return MY_COMPANIES
+  elif query.data == 'registered_companies':
+    print("registered companies")
+  elif query.data == 'add_company':
+    return ADD_COMPANY_STEP_ONE
+  else:
+    await query.edit_message_text(
+      text="<b>Invalid choice. Please try again.</b>"
+    )
+    return COMPANIES
+
   await update.message.reply_html(
-    text=f"<b>My Startups:</b> view and manage startups you have already registered\n\n"
-          "<b>Add Startup:</b>: register a new startup\n\n",
+    text=f"...coming soon...",
   )
   
   return BUSINESS_TYPE_CHOICE
 
-async def companies(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def add_company_back(update: Update, context: ContextTypes.DEFAULT_TYPE):
   await update.message.reply_html(
-    text=f"<b>My Companies:</b> view and manage companies you have already registered\n\n"
-          "<b>Add Company:</b>: register a new company\n\n",
+    text=f"",
+    reply_markup=InlineKeyboardMarkup(backOrCancelKeyboard),
+    parse_mode="HTML",
+  )
+  return ADD_COMPANY_STEP_FIVE
+  pass
+
+async def add_company_cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):  
+  await update.message.reply_html(
+    text=f"",
+    reply_markup=InlineKeyboardMarkup(backOrCancelKeyboard),
+    parse_mode="HTML",
+  )
+  return ADD_COMPANY_STEP_FIVE
+  pass
+
+async def add_company_step_one(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  recruiterTypeKeyboard = [
+    [
+      InlineKeyboardButton('General Manager', callback_data='general_manager'),
+      InlineKeyboardButton('Recruiter', callback_data='recruiter')
+    ],
+    [InlineKeyboardButton('Back', callback_data='back_to_business_type_choice')],
+  ]
+
+  await update.message.reply_html(
+    text=f"Please choose how you would like to register. \n\n"
+          "<b>General manager</b> (the person on the Trade license) \n"
+          "\t- General Manager ID photo/pdf \n"
+          "\t- Trade license photo/pdf \n\n"
+
+          "<b>Recruiter</b> (a person who works for the company) \n"
+          "\t- Recruiter ID photo/pdf \n"
+          "\t- Letter of authorization to register and recruit on behalf of the company (with the company stamp) photo/pdf \n"
+          "\t- Trade license photo/pdf \n\n"
+          
+          "<i>* files can be pdf or photo</i>",
+    reply_markup=InlineKeyboardMarkup(recruiterTypeKeyboard),
+    parse_mode="HTML"
+  )
+  
+  return ADD_COMPANY_STEP_TWO
+
+async def add_company_step_two(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  query = update.callback_query
+  await query.answer()
+  
+  backOrCancelKeyboard = [
+    [
+      InlineKeyboardButton('Back', callback_data='back_one_step'),
+      InlineKeyboardButton('Cancel', callback_data='cancel')
+    ],
+  ]
+  
+  if query.data == 'back_to_business_type_choice':
+    return ADD_COMPANY_STEP_ONE
+  elif query.data == 'general_manager':
+    await query.edit_message_text(
+      text=f"[1/3] <b>Please enter the name of your company</b>\n\n"
+            "<i>* The name of the company should match the name on the trade license</i>",
+      reply_markup=InlineKeyboardMarkup(backOrCancelKeyboard),
+      parse_mode="HTML",
+    )
+    return ADD_COMPANY_STEP_THREE
+  elif query.data == 'recruiter':
+    await query.edit_message_text(
+      text=f"[1/4] <b>Please enter the name of your company</b>\n\n"
+            "<i>* The name of the company should match the name on the trade license</i>",
+      reply_markup=InlineKeyboardMarkup(backOrCancelKeyboard),
+      parse_mode="HTML",
+    )
+    return ADD_COMPANY_STEP_THREE
+  else:
+    await query.edit_message_text(
+      text="<b>Invalid choice. Please try again.</b>"
+    )
+    return ADD_COMPANY_STEP_TWO
+
+async def add_company_step_three(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  context.user_data['company_name'] = update.message.text
+  
+  backOrCancelKeyboard = [
+    [
+      InlineKeyboardButton('Back', callback_data='back_one_step'),
+      InlineKeyboardButton('Cancel', callback_data='cancel')
+    ],
+  ]
+  
+  await update.message.reply_html(
+    text=f"[2/3] <b>Please upload your trade license as a photo/pdf</b>\n\n"
+          "<i>* file can be pdf or photo \n\n</i>"
+          "<i>* This information will ONLY be used to verify that your company is a legitimate business and will NOT be made public.</i>",
+    reply_markup=InlineKeyboardMarkup(backOrCancelKeyboard),
+    parse_mode="HTML",
+  )
+  return ADD_COMPANY_STEP_FOUR
+
+async def add_company_step_four(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  license_file = await update.message.photo[-1].get_file()
+  context.user_data['company_license'] = license_file.file_id
+  
+  backOrCancelKeyboard = [
+    [
+      InlineKeyboardButton('Back', callback_data='back_one_step'),
+      InlineKeyboardButton('Cancel', callback_data='cancel')
+    ],
+  ]
+  
+  await update.message.reply_html(
+    text=f"[3/3] <b>Please upload the general manager’s ID as a photo/pdf</b>\n\n"
+          "<i>Note: The general manager is the person that is on the trade license. \n\n</i>"
+          "<i>* ID can be driver’s license, passport, company ID or residence/kebele ID \n\n</i>"
+          "<i>* file can be pdf or photo \n\n</i>"
+          "<i>* This information will ONLY be used to verify that your company is a legitimate business and will NOT be made public.</i>",
+    reply_markup=InlineKeyboardMarkup(backOrCancelKeyboard),
+    parse_mode="HTML",
+  )
+  return ADD_COMPANY_STEP_FIVE
+
+async def add_company_step_five(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  pass
+
+
+async def startups(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  await update.message.reply_html(
+    text=f"...coming soon..."
   )
   
   return BUSINESS_TYPE_CHOICE
@@ -320,8 +467,15 @@ def main() -> None:
   company_conv_handler = ConversationHandler(
     entry_points=[CallbackQueryHandler(my_companies, '^my_companies$')],
     states={
-      BUSINESS_TYPE_CHOICE: [CallbackQueryHandler(business_type_choice, 'business_type_choice')],
+      BUSINESS_TYPE_CHOICE: [CallbackQueryHandler(business_type_choice)],
+      MY_COMPANIES: [MessageHandler(filters.TEXT & (~filters.COMMAND), my_companies)],
       COMPANIES: [MessageHandler(filters.TEXT & (~filters.COMMAND), companies)],
+      ADD_COMPANY_STEP_ONE: [MessageHandler(filters.TEXT & (~filters.COMMAND), add_company_step_one)],
+      ADD_COMPANY_STEP_TWO: [MessageHandler(filters.TEXT & (~filters.COMMAND), add_company_step_two)],
+      ADD_COMPANY_STEP_THREE: [MessageHandler(filters.TEXT & (~filters.COMMAND), add_company_step_three)],
+      ADD_COMPANY_STEP_FOUR: [MessageHandler(filters.TEXT & (~filters.COMMAND), add_company_step_four)],
+      ADD_COMPANY_STEP_FIVE: [MessageHandler(filters.TEXT & (~filters.COMMAND), add_company_step_five)],
+      
       STARTUPS: [MessageHandler(filters.TEXT & (~filters.COMMAND), startups)],
     },
     fallbacks=[CommandHandler("cancel", cancel_command)],
